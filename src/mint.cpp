b9ffc42d40f8f9cdf1f196bc16cb46784b40f856
Valentin-Buchnev <buchnev.valentin@gmail.com>
2023-12-04 21:00:23 +0300

    Update mint.cpp

--- src/mint.cpp
+++ src/mint.cpp
@@ -1,6 +1,6 @@
 class mint {
 private:
-    static const int MOD = 1e9 + 7;
+    static const int MOD = 998244353;
     int val;
 public:
     mint (ll x=0) {
@@ -11,41 +11,107 @@ public:
     mint (const mint& p) {
         val = p.val;
     }
-    mint operator +(mint p) {
+    const mint operator +(const mint& p) const {
         return mint(val + p.val);
     }
-    mint operator -(mint p) {
+    const mint operator +(ll p) const {
+        return mint(val + p);
+    }
+    const mint operator -(const mint& p) const {
         return mint(val - p.val);
     }
-    mint operator *(mint p) {
+    const mint operator -(ll p) const {
+        return mint(val - p);
+    }
+    const mint operator *(const mint& p) const {
         return mint(val * 1LL * p.val);  
     }
-    mint operator /(mint p) {
+    const mint operator *(ll p) const {
+        p %= MOD;
+        return mint(val * 1LL * p);  
+    }
+    const mint operator /(const mint& p) const {
         return mint(val) * 1LL * binpow(p, MOD - 2);
     }
-    void operator +=(mint p) {
-        *this = *this + p;
+    const mint operator /(ll p) const {
+        return mint(val) * 1LL * binpow(mint(p), MOD - 2);
     }
-    void operator -=(mint p) {
-        *this = *this - p;
+    mint& operator =(const mint& p) {
+        val = p.val;
+        return *this;
     }
-    void operator *=(mint p) {
-        *this = *this * p;
+    mint& operator =(ll p) {
+        val = p;
+        return *this;
     }
-    void operator /=(mint p) {
-        *this = *this / p;
+    mint& operator +=(const mint& p) {
+        return *this = *this + p;
     }
-    mint binpow(mint p, int d) {
-        if (d == 0)
-            return mint(1);
-        if (d % 2 == 0) {
-            mint x = binpow(p, d / 2);
-            return x * x;
+    mint& operator +=(ll p) {
+        return *this = *this + mint(p);
+    }
+    mint& operator -=(const mint& p) {
+        return *this = *this - p;
+    }
+    mint& operator -=(ll p) {
+        return *this = *this - mint(p);
+    }
+    mint& operator *=(const mint& p) {
+        return *this = *this * p;
+    }
+    mint& operator *=(ll p) {
+        return *this = *this * mint(p);
+    }
+    mint& operator /=(const mint& p) {
+        return *this = *this / p;
+    }
+    mint& operator /=(ll p) {
+        return *this = *this / mint(p);
+    }
+    bool operator ==(const mint& p) const {
+        return val == p.val;
+    }
+    bool operator ==(ll p) const {
+        return val == p;
+    }
+    bool operator !=(const mint& p) const {
+        return val != p.val;
+    }
+    bool operator !=(ll p) const {
+        return val != p;
+    }
+    mint binpow(mint p, ll d) const {
+        d %= MOD - 1;
+        mint ans(1);
+        while (d) {
+            if (d & 1)
+                ans *= p;
+            p *= p;
+            d >>= 1;
         }
-        return p * binpow(p, d - 1);
+        return ans;
+    }
+    mint binpow(ll p, ll d) {
+        return binpow(mint(p), d);
     }
     friend ostream &operator<<(ostream &out, mint p) {
         out << p.val;
         return out;
     }
 };
+
+const mint operator +(ll a, const mint& p) {
+    return p + a;
+}
+
+const mint operator -(ll a, const mint& p) {
+    return p - a;
+}
+
+const mint operator /(ll a, const mint& p) {
+    return p / a;
+}
+
+const mint operator *(ll a, const mint& p) {
+    return p * a;
+}
